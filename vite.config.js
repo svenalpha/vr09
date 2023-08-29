51e56fadd3f94631ab9ee23f097dabb5ff8f4199
svenalpha <svalphaforsale@yahoo.com>
2023-08-29 12:12:42 +0700

    message

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/vr09/',  
   plugins: [react()],
   build: {
     minify: false,

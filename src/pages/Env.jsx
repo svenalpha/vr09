a66701ef3692334a8573d149246ceda473caab8f
svenalpha <svalphaforsale@yahoo.com>
2023-09-18 14:25:46 +0700

    message

--- src/pages/Env.jsx
+++ src/pages/Env.jsx
@@ -1,5 +1,5 @@
 export default function Env() {
-  let msg = 'default message here in Env post removed Navigate no1'
+  let msg = 'default message here in Env post removed Navigate no2'
   try {
     msg = process.env.MY_CUSTOM_SECRET || msg
   } catch {}

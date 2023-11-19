2bf83117a2fae0988d5d2b03d426fdda66a43db3
svenalpha <svalphaforsale@yahoo.com>
2023-11-19 11:18:37 +0700

    message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { multiplyAndAdd } from '../multiply'
 export default function Home() {
   return (
     <>
-      <h1>Home    No8 17:59 18/11/2023 </h1>
+      <h1>Home    No9 11:15 19/11/2023 </h1>
       <div>{addAndMultiply(1, 2, 3)}</div>
       <div>{multiplyAndAdd(1, 2, 3)}</div>
     </>

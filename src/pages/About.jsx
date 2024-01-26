70ff87330c291d1c03917628088908cd0148be2a
svenalpha <svalphaforsale@yahoo.com>
2024-01-26 16:38:38 +0700

    message

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ import axios from "axios";
 export default function About() {
   const [userz,setUserz]=useState([]);
 useEffect(() =>{ 
-  axios.get("/apix/userz")
+  axios.get("/userz")
   .then(res =>{setUserz(res.data)
               }
        )
